8ff87b7dd5cb716eec196a395af3a6d14130b9ca
Murali061 <51677520+Murali061@users.noreply.github.com>
2019-11-25 00:32:18 +0530

    Added required components, services, models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
+import { SearchResultComponent } from './components/search-result/search-result.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientJsonpModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { AlertModule } from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    SearchResultComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/search/search.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import {ResultModel} from '../../model/result-model';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {AppService} from '../../services/app.service';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SearchComponent implements OnInit {
+  private appTitle = 'Search Latitude and Longitude';
+  private processedResult: ResultModel;
+  private cityForm: FormGroup;
+  private loading = false;
+  private formError = false;
+  constructor(private appService: AppService) {}
+  public ngOnInit(): void {
+    this.cityForm = new FormGroup({
+      cityName: new FormControl('', Validators.required)
+    });
+  }
+  public onSubmit(): void {
+    this.resetValues();
+    const cityName = this.cityForm.get('cityName').value || '';
+    if (cityName.trim() !== '' && cityName !== undefined) {
+      this.loading = true;
+      this.appService.getLatitudeAndLongitudes(cityName).subscribe((data) => {
+        this.processedResult = data;
+        this.loading = false;
+      });
+      this.formError = false;
+    } else {
+      this.formError = true;
+    }
+  }
+  private resetValues(): void {
+    this.processedResult = null;
+  }
+}
