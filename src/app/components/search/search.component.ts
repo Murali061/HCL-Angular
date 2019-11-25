c09d538f6de3117bcaf58ecaa327956d49debc35
Murali061 <51677520+Murali061@users.noreply.github.com>
2019-11-25 22:16:19 +0530

    Removing the result section when the user clears the search input

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -14,27 +14,41 @@ export class SearchComponent implements OnInit {
   private cityForm: FormGroup;
   private loading = false;
   private formError = false;
+  private showResult = false;
+  private inputClearedFirstTime = false;
   constructor(private appService: AppService) {}
   public ngOnInit(): void {
     this.cityForm = new FormGroup({
       cityName: new FormControl('', Validators.required)
     });
   }
+  public onSearchChange() {
+    this.cityForm.get('cityName').valueChanges.subscribe((value) => {
+      if (!value && !this.inputClearedFirstTime && this.showResult) {
+        this.showResult = false;
+        this.inputClearedFirstTime = true;
+      }
+    });
+     }
   public onSubmit(): void {
     this.resetValues();
     const cityName = this.cityForm.get('cityName').value || '';
     if (cityName.trim() !== '' && cityName !== undefined) {
       this.loading = true;
+      this.inputClearedFirstTime = false;
       this.appService.getLatitudeAndLongitudes(cityName).subscribe((data) => {
         this.processedResult = data;
         this.loading = false;
+        this.showResult = true;
       });
       this.formError = false;
     } else {
       this.formError = true;
+      this.showResult = false;
     }
   }
   private resetValues(): void {
     this.processedResult = null;
+    this.showResult = false;
   }
 }
